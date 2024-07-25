94024555d643ecf3056d856b3857ecbc3e844ec9
gladysruslim <gladysruslim22@gmail.com>
2024-07-25 14:39:50 +0700

    add conversations and chat page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:homiepet/pages/conversation.dart';
 import 'pages/login.dart';
 import 'pages/register.dart';
 import 'pages/preference.dart';
@@ -8,21 +9,27 @@ import 'pages/profile.dart';
 import 'pages/edit_profile.dart';
 import 'pages/change_password.dart';
 import 'pages/homepage.dart';
+import 'pages/conversation.dart';
 
 void main() => runApp(
   MaterialApp(
     theme: ThemeData(fontFamily: 'SanFrancisco'),
-    initialRoute: '/splash',
+    initialRoute: '/conversation',
     routes: {
       '/splash' : (context) => Splash(),
       '/preference' : (context) => Preference(),
       '/login' : (context) => LoginPage(),
       '/register' : (context) => RegisterPage(),
-      '/map' : (context) => Map(),
+      '/map' : (context) => Map(
+        onShareLocation: (location) {
+          Navigator.pop(context, location); // Return the location to the previous page
+        },
+      ),
       '/profile' : (context) => ProfilePage(),
       '/edit_profile' : (context) => EditProfilePage(),
       '/change_password' : (context) => ChangePasswordPage(),
       '/homepage' : (context) => HomePage(),
+      '/conversation': (context) => Conversation(),
 
 
     },
